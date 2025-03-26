e728704d7ca4a5e3108f344640140920c74b6f67
Razvan Macovei <razvanmc15@gmail.com>
2025-03-26 10:21:48 +0200

    mesaj de reparat app.config

--- Program.cs
+++ Program.cs
@@ -8,9 +8,7 @@ namespace Login_and_Register_System
 {
     internal static class Program
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
